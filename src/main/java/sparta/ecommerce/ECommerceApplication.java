e238dea7a037dcaae5e5a815cd318545fba5e4ff
Jeon Hyejin <107957837+jane5am@users.noreply.github.com>
2024-07-05 23:14:18 +0900

    Setting: 루트 프로젝트에 유레카 서버 설정

--- src/main/java/sparta/ecommerce/ECommerceApplication.java
+++ src/main/java/sparta/ecommerce/ECommerceApplication.java
@@ -2,12 +2,14 @@ package sparta.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ECommerceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ECommerceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
 
 }
