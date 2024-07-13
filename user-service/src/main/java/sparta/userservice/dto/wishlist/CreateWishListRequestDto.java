8f8430d655e342c04ef0529dba4ae515127bfd7b
Jeon Hyejin <hyejda00@naver.com>
2024-07-14 02:48:43 +0900

    Fix: 위시리스트 생성 기능 타입 수정

--- user-service/src/main/java/sparta/userservice/dto/wishlist/CreateWishListRequestDto.java
+++ user-service/src/main/java/sparta/userservice/dto/wishlist/CreateWishListRequestDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateWishListRequestDto {
-    private String productId;
+    private int productId;
     private int quantity;
 }
