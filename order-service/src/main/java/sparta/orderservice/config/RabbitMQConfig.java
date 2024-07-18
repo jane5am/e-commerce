07573c73bfe437003d50ea23d7c6d21ff9ed5f4e
Jeon Hyejin <107957837+jane5am@users.noreply.github.com>
2024-07-18 10:53:05 +0900

    Feat: RabbitMQ에서 사용할 큐 정의, 등록

--- order-service/src/main/java/sparta/orderservice/config/RabbitMQConfig.java
+++ order-service/src/main/java/sparta/orderservice/config/RabbitMQConfig.java
@@ -6,66 +6,54 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sparta.orderservice.client.ProductServiceClient;
 import sparta.orderservice.listener.OrderMessageListener;
-import sparta.orderservice.listener.ProductUpdateListener;
+import sparta.orderservice.listener.ProductPriceResponseListener;
 
 @Configuration
 public class RabbitMQConfig {
 
-    // 큐 이름을 상수로 정의
     public static final String ORDER_QUEUE = "order-queue";
-
     public static final String PRODUCT_UPDATE_QUEUE = "product-update-queue";
-
     public static final String PRODUCT_PRICE_REQUEST_QUEUE = "product-price-request-queue";
     public static final String PRODUCT_PRICE_RESPONSE_QUEUE = "product-price-response-queue";
 
-    // 큐를 생성하는 빈 정의
     @Bean
-    public Queue queue() {
-        return new Queue(ORDER_QUEUE, false); // 두 번째 매개변수는 durable 옵션으로, false이면 메시지가 메모리에만 저장됨
+    public Queue orderQueue() {
+        return new Queue(ORDER_QUEUE, true);
     }
 
     @Bean
     public Queue productUpdateQueue() {
-        System.out.println("2222222222222");
-        return new Queue(PRODUCT_UPDATE_QUEUE, false);
+        return new Queue(PRODUCT_UPDATE_QUEUE, true);
     }
 
-//    @Bean
-//    public Queue productPriceRequestQueue() {
-//        return new Queue(PRODUCT_PRICE_REQUEST_QUEUE, false);
-//    }
-//
-//    @Bean
-//    public Queue productPriceResponseQueue() {
-//        return new Queue(PRODUCT_PRICE_RESPONSE_QUEUE, false);
-//    }
+    @Bean
+    public Queue productPriceRequestQueue() {
+        return new Queue(PRODUCT_PRICE_REQUEST_QUEUE, true);
+    }
 
-    // RabbitTemplate 빈 정의
+    @Bean
+    public Queue productPriceResponseQueue() {
+        return new Queue(PRODUCT_PRICE_RESPONSE_QUEUE, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        System.out.println("3333333333333");
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter()); // 메시지 컨버터를 JSON으로 설정
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
     }
 
-    // Jackson2JsonMessageConverter 빈 정의
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        System.out.println("444444444444");
-        return new Jackson2JsonMessageConverter(); // 메시지를 JSON 형식으로 변환하는 컨버터
-    }
-
-    // 메시지 리스너 컨테이너 빈 정의
     @Bean
     public SimpleMessageListenerContainer orderContainer(ConnectionFactory connectionFactory,
-                                                         @Qualifier("listenerAdapter") MessageListenerAdapter orderListenerAdapter) {
+                                                         MessageListenerAdapter orderListenerAdapter) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(ORDER_QUEUE);
@@ -74,45 +62,26 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public SimpleMessageListenerContainer productUpdateContainer(ConnectionFactory connectionFactory,
-                                                                 @Qualifier("productUpdateListenerAdapter") MessageListenerAdapter productUpdateListenerAdapter) {
+    public MessageListenerAdapter orderListenerAdapter(OrderMessageListener listener, Jackson2JsonMessageConverter converter) {
+        MessageListenerAdapter adapter = new MessageListenerAdapter(listener, "receiveOrderMessage");
+        adapter.setMessageConverter(converter);
+        return adapter;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer productPriceResponseContainer(ConnectionFactory connectionFactory,
+                                                                        MessageListenerAdapter productPriceResponseListenerAdapter) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(PRODUCT_UPDATE_QUEUE);
-        container.setMessageListener(productUpdateListenerAdapter);
+        container.setQueueNames(PRODUCT_PRICE_RESPONSE_QUEUE);
+        container.setMessageListener(productPriceResponseListenerAdapter);
         return container;
     }
 
-//    @Bean
-//    public SimpleMessageListenerContainer productPriceRequestContainer(ConnectionFactory connectionFactory,
-//                                                                       @Qualifier("productPriceRequestListenerAdapter") MessageListenerAdapter productPriceRequestListenerAdapter) {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        container.setQueueNames(PRODUCT_PRICE_REQUEST_QUEUE);
-//        container.setMessageListener(productPriceRequestListenerAdapter);
-//        return container;
-//    }
-
-    // MessageListenerAdapter 빈 정의
     @Bean
-    public MessageListenerAdapter listenerAdapter(OrderMessageListener receiver) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(receiver, "receiveMessage"); // OrderMessageListener의 receiveMessage 메서드를 호출하도록 설정
-        adapter.setMessageConverter(jsonMessageConverter()); // 메시지 컨버터를 JSON으로 설정
+    public MessageListenerAdapter productPriceResponseListenerAdapter(ProductPriceResponseListener listener) {
+        MessageListenerAdapter adapter = new MessageListenerAdapter(listener, "onMessage");
+        adapter.setMessageConverter(jsonMessageConverter());
         return adapter;
     }
-
-    @Bean
-    public MessageListenerAdapter productUpdateListenerAdapter(ProductUpdateListener productUpdateListener) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(productUpdateListener, "receiveMessage");
-        adapter.setMessageConverter(jsonMessageConverter()); // 메시지 컨버터를 JSON으로 설정
-        return adapter;
-    }
-
-    @Bean
-    public MessageListenerAdapter getProductPriceListenerAdapter(ProductUpdateListener productUpdateListener) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(productUpdateListener, "receiveMessage2");
-        adapter.setMessageConverter(jsonMessageConverter()); // 메시지 컨버터를 JSON으로 설정
-        return adapter;
-    }
-
 }
