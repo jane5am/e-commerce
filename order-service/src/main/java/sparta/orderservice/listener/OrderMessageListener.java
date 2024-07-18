beb39d4419af84ab8e3682a833c18857142161a7
Jeon Hyejin <107957837+jane5am@users.noreply.github.com>
2024-07-18 10:53:53 +0900

    Feat: 오더 메시지를 듣고 소비하는 오더메시지 리스너 예외처리 추가

--- order-service/src/main/java/sparta/orderservice/listener/OrderMessageListener.java
+++ order-service/src/main/java/sparta/orderservice/listener/OrderMessageListener.java
@@ -1,18 +1,23 @@
 package sparta.orderservice.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import sparta.orderservice.message.OrderMessage;
 import sparta.orderservice.order.OrderService;
 
-@Service
+@Component
 public class OrderMessageListener {
 
     @Autowired
     private OrderService orderService;
 
-    public void receiveMessage(OrderMessage orderMessage) {
-        System.out.println("rabbitMQ Listener");
-        orderService.createOrder(orderMessage.getUserId(), orderMessage.getOrderItems());
+    public void receiveOrderMessage(OrderMessage orderMessage) {
+        try {
+            orderService.createOrder(orderMessage.getUserId(), orderMessage.getOrderItems());
+        } catch (Exception e) {
+            // 예외 로그 출력
+            System.err.println("Failed to create order: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
