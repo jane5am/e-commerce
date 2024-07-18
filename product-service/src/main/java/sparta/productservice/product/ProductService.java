7de2c2abd6c8e9f844ff24e62f73b77818b81066
Jeon Hyejin <107957837+jane5am@users.noreply.github.com>
2024-07-18 10:57:58 +0900

    Refactor: Product-service 예외처리 추가, 공통 Dto를 common 모듈로 이동

--- product-service/src/main/java/sparta/productservice/product/ProductService.java
+++ product-service/src/main/java/sparta/productservice/product/ProductService.java
@@ -1,11 +1,11 @@
 package sparta.productservice.product;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import sparta.common.messages.CreateOrderDto;
 import sparta.productservice.domain.Product;
 import sparta.productservice.dto.product.CreateProductDto;
-import sparta.productservice.dto.product.ProductDto;
-import sparta.orderservice.dto.CreateOrderDto;
 import sparta.productservice.dto.product.PutProductDto;
 
 import java.time.LocalDateTime;
@@ -13,6 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -22,15 +23,18 @@ public class ProductService {
 
 
     public void updateStock(CreateOrderDto createOrderDto) {
+        log.info("Starting updateStock for productId: " + createOrderDto.getProductId());
         Product product = productRepository.findById(createOrderDto.getProductId())
                 .orElseThrow(() -> new RuntimeException("Product not found"));
         int newStock = product.getQuantity() - createOrderDto.getQuantity();
         if (newStock < 0) {
+            log.error("Not enough stock available for productId: " + createOrderDto.getProductId());
             throw new RuntimeException("Not enough stock available");
         }
         product.setQuantity(newStock);
         product.setUpdatedAt(LocalDateTime.now());
         productRepository.save(product);
+        log.info("Finished updateStock for productId: " + createOrderDto.getProductId());
     }
 
     public void restoreStock(CreateOrderDto createOrderDto) {
@@ -46,11 +50,13 @@ public class ProductService {
     }
 
     public int getProductPrice(int productId) {
+        log.info("Starting getProductPrice for productId: " + productId);
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
-        return product.getPrice();
+        int price = product.getPrice();
+        log.info("Finished getProductPrice for productId: " + productId + ", price: " + price);
+        return price;
     }
-
     public List<Product> getProductsByIds(List<Integer> productIds) {
         return productRepository.findAllById(productIds);
     }
